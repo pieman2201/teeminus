f803809ca89fc9d6648ccd68041a5359d6e2ca9f
Ishan Kamat <ishankamat123@gmail.com>
2022-05-16 13:26:43 -0500

    implement display

--- docs/script.js
+++ docs/script.js
@@ -8,16 +8,81 @@ function decode(s) {
 
 function show() {
     const params = new URLSearchParams(window.location.search);
-    if (params.has('d')) {
+    if (params.has('i')) {
         // show display
         document.getElementById("display").style.display = "flex";
         document.getElementById("create").style.display = "none";
 
-        console.log(decode(params.get('d')));
+        // set name and pass date
+        var value = decode(params.get('i'));
+        document.getElementById("dsp-name").textContent = value.n;
+
+        display(new Date(value.d));
+        setInterval(() => display(new Date(value.d)), 100);
 
     } else {
+        // show creation ui
         document.getElementById("display").style.display = "none";
         document.getElementById("create").style.display = "flex";
-
     }
+}
+
+function create() {
+    const dateInput = document.getElementById("inp-date");
+    const timeInput = document.getElementById("inp-time");
+    const nameInput = document.getElementById("inp-name");
+
+    var cDate = dateInput.valueAsDate;
+    var cTime = timeInput.valueAsDate;
+
+    if (cDate == null) {
+        cDate = new Date();
+        cDate.setTime(cDate.getTime() - cDate.getTimezoneOffset() * 60 * 1000);
+    }
+
+    if (cTime != null) {
+        cDate.setUTCHours(cTime.getUTCHours());
+        cDate.setUTCMinutes(cTime.getUTCMinutes());
+        cDate.setUTCSeconds(cTime.getUTCSeconds());
+    }
+
+    cDate.setUTCMilliseconds(0);
+    cDate.setTime(cDate.getTime() + cDate.getTimezoneOffset() * 60 * 1000);
+
+    console.log(cDate);
+
+    var cName = nameInput.value;
+    console.log(cName);
+
+    var baseURL = window.location.origin + window.location.pathname;
+    var newParams = new URLSearchParams({
+        i: encode({
+            d: cDate,
+            n: cName
+        })
+    });
+    window.location.href = baseURL + "?" + newParams.toString();
+}
+
+function display(d) {
+    var now = new Date();
+    var rem;
+    if (d.getTime() >= now.getTime()) {
+        rem = d - now;
+        document.getElementById("preface").textContent = "-";
+    } else {
+        rem = now - d;
+        document.getElementById("preface").textContent = "+";
+    }
+    rem = Math.floor(rem / 1000);
+
+    var seconds = rem % 60; rem -= seconds; rem /= 60;
+    var minutes = rem % 60; rem -= minutes; rem /= 60;
+    var hours = rem % 24; rem -= hours; rem /= 24;
+    var days = rem;
+
+    document.getElementById("clock-days").textContent = days;
+    document.getElementById("clock-hours").textContent = hours;
+    document.getElementById("clock-minutes").textContent = minutes;
+    document.getElementById("clock-seconds").textContent = seconds;
 }
